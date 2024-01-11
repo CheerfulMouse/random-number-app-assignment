7bb033be7c5fba40e33de8c4b1f7df5566c7470f
CheerfulMouse <othello.senecharles@gmail.com>
2024-01-11 14:51:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# random-number-app-assignment
+ I made a console app that would print out 20 yes's or no's because I didn't want to survey anyone for an assignment
---
+++ random number printer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+Random rnd = new Random();
+string? readResult;
+string entry = "";
+bool validEntry = false;
+
+Console.WriteLine("press enter to generate 20 Yes or No's");
+readResult = Console.ReadLine();
+if (readResult != "")
+{
+    validEntry = true;
+}
+do
+{
+    for (int i = 0; i < 20; i++)
+    {
+        int result = rnd.Next();
+        int answer = (result % 2);
+        if (answer == 0)
+        {
+            Console.WriteLine((i + 1) + ": Yes");
+        }
+        else
+        {
+            Console.WriteLine((i + 1) + ": No");
+        }
+
+    }
+    Console.WriteLine("Would you like to continue? Press Y to generate another list, or press N to finish");
+    readResult = Console.ReadLine();
+    if (readResult != "")
+    {
+        entry = readResult.ToLower();
+        if (entry == "y")
+        {
+            validEntry = true;
+        }
+        else if (entry == "n")
+        {
+            validEntry = false;
+        }
+    }
+} while (validEntry == true);
+
+
+
+if (validEntry == false)
+{
+    Console.WriteLine("Thank you for using this App. press Enter to Close");
+}
